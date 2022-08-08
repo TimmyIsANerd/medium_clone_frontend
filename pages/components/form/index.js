643926c12c35538550460a54a664aacaf4cd896d
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-08-08 19:48:04 +0100

    started working on login component

---
+++ pages/components/form/index.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+`;
+
+const LoginForm = () => {
+  return (
+    <>
+      <Container>
+        Login Form
+      </Container>
+    </>
+  );
+};
+
+export default LoginForm;
